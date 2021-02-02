06dbe8452363a4c5dd96f9bc38d8d62239bcdd03
watanabe-kyo <watanabe.kyo.infratop@gmail.com>
2021-02-02 06:21:47 +0000

    Mylistにボタンを追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'hVlG3Tov0nPHGXw0r2PmsHmHA', 'alryXE52XTmacvYsFwKDVTy02rLzSnMwVzL3H6btWNQq7w5D8N'
+  provider :twitter, 'Yhq7vlmb989lACy07hV6Aj4eM', 'Uf2hdBhoG5R4E4FhEcP5A8vWMR1j9Iv46UM1ZhBmGDbIOWmuIq'
 end
