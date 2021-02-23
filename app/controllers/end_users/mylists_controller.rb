18ffc57eb4b7abe8bfa5282307b7ef74e1aa42aa
watanabe-kyo <watanabe.kyo.infratop@gmail.com>
2021-02-23 05:43:58 +0000

    heroku

--- app/controllers/end_users/mylists_controller.rb
+++ app/controllers/end_users/mylists_controller.rb
@@ -1,6 +1,30 @@
 class EndUsers::MylistsController < ApplicationController
     before_action :authenticate_end_user!
 
+    # def create
+    #     @mylist = Mylist.where(end_user_id: params[:id])
+    #     @list = Mylist.new
+    #     @list.end_user_id = current_end_user.id
+    #     @list.track_id = params[:track_id]
+    #     @list.collection_id = params[:collection_id]
+    #     @list.save
+    # end
+
+    # def destroy
+    #     @list = Mylist.find_by(end_user_id: current_end_user, track_id: params[:track_id], collection_id: params[:collection_id])
+    #     @list.destroy
+    #     if params[:id] != nil
+    #         @end_user = EndUser.find(params[:id])
+    #         @mylists = @end_user.mylists
+    #     end
+    # end
+
+    # def show
+    #     @mylist = Mylist.where(end_user_id: params[:id])
+    #     @end_user = EndUser.find(params[:id])
+    #     @mylists = @end_user.mylists
+    # end
+
     def create
         @mylist = Mylist.where(end_user_id: params[:id])
         @list = Mylist.new
@@ -12,15 +36,17 @@ class EndUsers::MylistsController < ApplicationController
 
     def destroy
         @list = Mylist.find_by(end_user_id: current_end_user, track_id: params[:track_id], collection_id: params[:collection_id])
+
+        # if EndUser.find(params[:id]) != nil
+        #     @end_user = EndUser.find(params[:id])
+        #     @mylists = @end_user.mylists
+        #     binding.pry
+        # end
         @list.destroy
-        if params[:id] != nil
-            @end_user = EndUser.find(params[:id])
-            @mylists = @end_user.mylists
-        end
     end
 
     def show
-        @mylist = Mylist.where(end_user_id: params[:id])
+        @mylist = Mylist.find_by(end_user_id: current_end_user.id)
         @end_user = EndUser.find(params[:id])
         @mylists = @end_user.mylists
     end
