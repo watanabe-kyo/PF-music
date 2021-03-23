c95b268e329560aa8869b52f6f60efc8584a91f0
watanabe-kyo <watanabe.kyo.infratop@gmail.com>
2021-03-23 03:46:08 +0000

    twitter login test-2

--- app/helpers/end_users/sessions_helper.rb
+++ app/helpers/end_users/sessions_helper.rb
@@ -1,19 +1,19 @@
 module EndUsers::SessionsHelper
   # 渡されたユーザーでログインする
-  # def log_in(end_user)
-  #   if end_user.uid
-  #     session[:uid] = end_user.uid
-  #   else
-  #     session[:uid] = end_user.id
-  #   end
-  # end
+  def log_in(end_user)
+    if end_user.uid != nil
+      session[:uid] = end_user.uid
+    else
+      session[:uid] = end_user.id
+    end
+  end
 
   # # 現在ログイン中のユーザーを返す (いる場合)
-  # def current_end_user
-  #   if session[:uid]
-  #     @current_end_user ||= EndUser.find_by(uid: session[:uid])
-  #   end
-  # end
+  def current_end_user
+    if session[:uid]
+      @current_end_user ||= EndUser.find_by(uid: session[:uid])
+    end
+  end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
